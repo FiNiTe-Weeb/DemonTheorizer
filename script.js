9327535eeb560f14e864fed7466a716d6e3602eb
FiNiTe-Weeb <101021772+FiNiTe-Weeb@users.noreply.github.com>
2022-03-05 23:26:43 +0200

    fix update thing
    b

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // @version      1.0.1
 // @homepage     https://github.com/FiNiTe-Weeb/DemonTheorizer
 // @updateURL    https://raw.githubusercontent.com/FiNiTe-Weeb/DemonTheorizer/main/script.js
+// @downloadURL  https://raw.githubusercontent.com/FiNiTe-Weeb/DemonTheorizer/main/script.js
 // @description  How many list Geometry Dash Pointercrate official demonlist points would I have if I got x% on y demon?????
 // @author       FiNiTe#8429 on Discord
 // @match        https://pointercrate.com/demonlist/statsviewer/
